b75a108d9b730f94a2aeb0ad8eeb5843057f692e
Daniel Sjöberg <danielsjoberg00@gmail.com>
2025-07-07 10:20:35 +0200

    updated documentation

--- plot_results.py
+++ plot_results.py
@@ -19,9 +19,7 @@ def ReadData(filename):
     return(theta, ff_theta, ff_phi, ff)
 
 for theta_degrees in [0, 10, 20, 30, 40, 50]: 
-    for wfactor in [10, 20]: 
-#for theta_degrees in [0, 30, 50]: 
-#    for wfactor in [30]:
+    for wfactor in [10, 20, 30]: 
         results_scatter = []
         results_antenna = []
         for air in [False, True]:
